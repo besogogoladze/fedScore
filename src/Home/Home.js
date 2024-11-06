eadbd8e4eb25f7b55c6d7aba7447ed36c03f20e1
besogogoladze <Gogoladzebeso8@gmail.com>
2024-11-06 11:52:33 +0100

    add # to links

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,9 +7,9 @@ function Home() {
       <div className="container">
         <img className="logo" alt="logo" src={logo} />
         <div className="homeButtonDiv">
-          <a href="/fedScore/EABXV">EAB XV</a>
-          <a href="/fedScore/Nantes">NANTES</a>
-          <a href="/fedScore/Havre">HAVRE</a>
+          <a href="#/fedScore/EABXV">EAB XV</a>
+          <a href="#/fedScore/Nantes">NANTES</a>
+          <a href="#/fedScore/Havre">HAVRE</a>
         </div>
       </div>
     </div>
